4c090146f127e5af9d327ec838b42c855a7d6077
Nivetha E <133072267+nivethaesn005@users.noreply.github.com>
2025-09-19 11:43:56 +0530

    Time: 2 ms (76.31%) | Memory: 59.72 MB (15.08%) - LeetSync

---
+++ 896 - Monotonic Array/896 - Monotonic Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean increase = true;
+        boolean decrease = true;
+
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i] > nums[i+1]){
+                increase = false;
+            }
+            if(nums[i] < nums[i+1]){
+                decrease = false;
+            }
+        }
+        return increase || decrease;
+    }
+}
